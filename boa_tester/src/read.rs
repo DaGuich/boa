09c4a4a07fe85b14befe0175527c9e61bffdc36a
Iban Eguia Moraza <razican@protonmail.ch>
2020-10-18 20:08:32 +0200

    Improved CI workflows
    This improves several things in the CI workflows:
    
     - More conformant Test262 result generation
     - Benchmarks should now show comments for all users
     - Added Test262 result comparison comments to Pull Requests

--- boa_tester/src/read.rs
+++ boa_tester/src/read.rs
@@ -1,6 +1,6 @@
 //! Module to read the list of test suites from disk.
 
-use super::{Harness, Locale, Phase, Test, TestSuite, CLI};
+use super::{Harness, Locale, Phase, Test, TestSuite};
 use fxhash::FxHashMap;
 use serde::Deserialize;
 use std::{fs, io, path::Path};
@@ -53,10 +53,10 @@ pub(super) enum TestFlag {
 }
 
 /// Reads the Test262 defined bindings.
-pub(super) fn read_harness() -> io::Result<Harness> {
+pub(super) fn read_harness(test262_path: &Path) -> io::Result<Harness> {
     let mut includes = FxHashMap::default();
 
-    for entry in fs::read_dir(CLI.test262_path().join("harness"))? {
+    for entry in fs::read_dir(test262_path.join("harness"))? {
         let entry = entry?;
         let file_name = entry.file_name();
         let file_name = file_name.to_string_lossy();
@@ -72,8 +72,8 @@ pub(super) fn read_harness() -> io::Result<Harness> {
             content.into_boxed_str(),
         );
     }
-    let assert = fs::read_to_string(CLI.test262_path().join("harness/assert.js"))?.into_boxed_str();
-    let sta = fs::read_to_string(CLI.test262_path().join("harness/sta.js"))?.into_boxed_str();
+    let assert = fs::read_to_string(test262_path.join("harness/assert.js"))?.into_boxed_str();
+    let sta = fs::read_to_string(test262_path.join("harness/sta.js"))?.into_boxed_str();
 
     Ok(Harness {
         assert,
